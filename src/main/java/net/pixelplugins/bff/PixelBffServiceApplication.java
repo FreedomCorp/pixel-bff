dffc1306b9e4df3d155122a2362758c47fdad8a1
asynccc <jvictor2018pb@gmail.com>
2022-10-06 10:20:58 -0300

    Initial commit

---
+++ src/main/java/net/pixelplugins/bff/PixelBffServiceApplication.java
@@ -0,0 +1,13 @@
+package net.pixelplugins.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelBffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelBffServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/pixelplugins/bff/PixelBffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pixelplugins.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelBffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
